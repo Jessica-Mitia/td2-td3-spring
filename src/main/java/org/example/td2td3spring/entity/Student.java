6481ca47d315132878c29c77c9e570dd22ec3574
Mitia Jessica <mitiah.jessica@gmail.com>
2026-03-19 19:58:47 +0300

    feat: add the class student

--- src/main/java/org/example/td2td3spring/entity/Student.java
+++ src/main/java/org/example/td2td3spring/entity/Student.java
@@ -1,4 +1,16 @@
 package org.example.td2td3spring.entity;
 
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class Student {
+    private String reference;
+    private String firstName;
+    private String lastName;
+    private int age;
 }
