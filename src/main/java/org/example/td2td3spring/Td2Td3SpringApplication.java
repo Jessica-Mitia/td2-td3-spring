c7e9e92e52fba35618a732024b78fce3f096ca3f
Mitia Jessica <mitiah.jessica@gmail.com>
2026-03-19 19:31:50 +0300

    initial commit

---
+++ src/main/java/org/example/td2td3spring/Td2Td3SpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.td2td3spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td2Td3SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Td2Td3SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/td2td3spring/Td2Td3SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.td2td3spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td2Td3SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
