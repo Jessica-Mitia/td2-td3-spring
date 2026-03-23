95a2f9df0b0eeeb111a803366e19d9b31fa2fe67
Mitia Jessica <mitiah.jessica@gmail.com>
2026-03-23 12:47:22 +0300

    feat: add the class studentController

--- src/main/java/org/example/td2td3spring/controller/StudentController.java
+++ src/main/java/org/example/td2td3spring/controller/StudentController.java
@@ -1,4 +1,39 @@
 package org.example.td2td3spring.controller;
 
+import org.example.td2td3spring.entity.Student;
+import org.example.td2td3spring.service.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping("/students")
+    public ResponseEntity<List<Student>> createStudent(@RequestBody List<Student> students) {
+        studentService.saveStudents(students);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(this.studentService.getAllStudents());
+    }
+
+    @GetMapping("/students")
+    public ResponseEntity<List<Student>> getAllStudents(@RequestHeader(value = "Accept", required = false) String accept) {
+        List<Student> students = studentService.getAllStudents();
+        if (accept == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        if (accept.equalsIgnoreCase("application/json") || accept.equalsIgnoreCase("text/plain")) {
+            return new ResponseEntity<>(students, HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+        }
+    }
 }
