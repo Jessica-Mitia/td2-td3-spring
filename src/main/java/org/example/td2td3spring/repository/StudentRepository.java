47d3b3b96b862ab0dc6db081a2d41e57160431d1
Mitia Jessica <mitiah.jessica@gmail.com>
2026-03-23 12:48:09 +0300

    feat: add the class studentRepository

--- src/main/java/org/example/td2td3spring/repository/StudentRepository.java
+++ src/main/java/org/example/td2td3spring/repository/StudentRepository.java
@@ -1,4 +1,23 @@
 package org.example.td2td3spring.repository;
 
+import org.example.td2td3spring.entity.Student;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class StudentRepository {
+    private List<Student> students;
+
+    public StudentRepository(List<Student> students) {
+        this.students = students;
+    }
+
+    public List<Student> findAll() {
+        return students;
+    }
+
+    public void save(Student student) {
+        students.add(student);
+    }
 }
