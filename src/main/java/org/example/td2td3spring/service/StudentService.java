763e70349f8e72e7f2a8bc7c0aee8a6cd699fde7
Mitia Jessica <mitiah.jessica@gmail.com>
2026-03-23 12:48:37 +0300

    feat: add the class studentService

--- src/main/java/org/example/td2td3spring/service/StudentService.java
+++ src/main/java/org/example/td2td3spring/service/StudentService.java
@@ -1,4 +1,26 @@
 package org.example.td2td3spring.service;
 
+import org.example.td2td3spring.entity.Student;
+import org.example.td2td3spring.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public void saveStudents(List<Student> students) {
+        for (Student student : students) {
+            this.studentRepository.save(student);
+        }
+    }
 }
