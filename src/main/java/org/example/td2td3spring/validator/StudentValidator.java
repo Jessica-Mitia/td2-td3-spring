9fd1df6fda1b515667a4e78288b53a27991da734
Mitia Jessica <mitiah.jessica@gmail.com>
2026-03-25 22:16:29 +0300

    feat: add the studentValidator class

--- src/main/java/org/example/td2td3spring/validator/StudentValidator.java
+++ src/main/java/org/example/td2td3spring/validator/StudentValidator.java
@@ -1,4 +1,20 @@
 package org.example.td2td3spring.validator;
 
+import org.example.td2td3spring.entity.Student;
+import org.example.td2td3spring.exception.BadRequestException;
+import org.springframework.stereotype.Component;
+
+@Component
 public class StudentValidator {
+    public void validate(Student student) {
+        if (student.getLastName() == null || student.getLastName().isBlank()) {
+            throw new BadRequestException("Last Name is required");
+        }
+        if (student.getFirstName() == null || student.getFirstName().isBlank()) {
+            throw new BadRequestException("First Name is required");
+        }
+        if (student.getReference() == null || student.getReference().isBlank()) {
+            throw new BadRequestException("Reference is required");
+        }
+    }
 }
