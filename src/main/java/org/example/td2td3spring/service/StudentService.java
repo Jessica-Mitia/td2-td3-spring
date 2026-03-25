2cbacf7006b16be0da71a7af37bd0aa82e6794d9
Mitia Jessica <mitiah.jessica@gmail.com>
2026-03-25 22:16:56 +0300

    update the studentService

--- src/main/java/org/example/td2td3spring/service/StudentService.java
+++ src/main/java/org/example/td2td3spring/service/StudentService.java
@@ -2,6 +2,7 @@ package org.example.td2td3spring.service;
 
 import org.example.td2td3spring.entity.Student;
 import org.example.td2td3spring.repository.StudentRepository;
+import org.example.td2td3spring.validator.StudentValidator;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,18 +10,26 @@ import java.util.List;
 @Service
 public class StudentService {
     private final StudentRepository studentRepository;
+    private final StudentValidator studentValidator;
 
-    public StudentService(StudentRepository studentRepository) {
+    public StudentService(StudentRepository studentRepository,
+                          StudentValidator studentValidator) {
         this.studentRepository = studentRepository;
+        this.studentValidator = studentValidator;
     }
 
     public List<Student> getAllStudents() {
         return studentRepository.findAll();
     }
 
+    public String getAllStudentsAsText() {
+        return studentRepository.findAll().toString();
+    }
+
     public void saveStudents(List<Student> students) {
         for (Student student : students) {
-            this.studentRepository.save(student);
+            studentValidator.validate(student);
+            studentRepository.save(student);
         }
     }
 }
